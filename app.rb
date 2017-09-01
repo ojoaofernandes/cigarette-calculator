c84f2881d7d7bf9cf5168755cb19856eac372e11
João Fernandes <ojoaofernandes@gmail.com>
2017-08-31 22:28:04 -0300

    Fix some typo

--- app.rb
+++ app.rb
@@ -7,7 +7,7 @@ puts 'How many cigarettes per day?'
 cigs_per_day = gets.chomp.to_i
 
 puts 'How much is a cigarette case? (Ex: $ 7,99 => 7.99)'
-cig_portfolio_price = gets.chomp.to_f
+cig_case_price = gets.chomp.to_f
 
-money = SmokingCigaretteCalculator.calculate years_count, cigs_per_day, cig_portfolio_price
+money = SmokingCigaretteCalculator.calculate years_count, cigs_per_day, cig_case_price
 puts "WOW! You spent $#{money} smoking."
--- src/SmokingCigaretteCalculator.rb
+++ src/SmokingCigaretteCalculator.rb
@@ -1,15 +1,15 @@
 class SmokingCigaretteCalculator
 
-    def self.calculate years_count, cigs_per_day, cig_portfolio_price
+    def self.calculate years_count, cigs_per_day, cig_case_price
         # aux vars
-        days_in_a_year     = 365
-        cigs_per_portfolio = 20
+        days_in_a_year = 365
+        cigs_per_case  = 20
 
         # do the math
-        total_days   = years_count * days_in_a_year
-        total_cigs   = total_days * cigs_per_day
-        money_expend = (total_cigs / cigs_per_portfolio) * cig_portfolio_price
-        return money_expend
+        total_days  = years_count * days_in_a_year
+        total_cigs  = total_days * cigs_per_day
+        money_spent = (total_cigs / cigs_per_case) * cig_case_price
+        return money_spent
     end
 
 end
