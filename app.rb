f65ea69657cd8ccf7a87329d9f71c802df86b788
João Fernandes <ojoaofernandes@gmail.com>
2017-08-31 22:22:04 -0300

    Show some messages to user before inputs

--- app.rb
+++ app.rb
@@ -1,7 +1,12 @@
 require './src/SmokingCigaretteCalculator'
 
+puts 'How many years of smoking?'
 years_count = gets.chomp.to_i
+
+puts 'How many cigarettes per day?'
 cigs_per_day = gets.chomp.to_i
+
+puts 'How much is a cigarette case? (Ex: $ 7,99 => 7.99)'
 cig_portfolio_price = gets.chomp.to_f
 
 money = SmokingCigaretteCalculator.calculate years_count, cigs_per_day, cig_portfolio_price
